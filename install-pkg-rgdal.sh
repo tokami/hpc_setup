aa6f5322e496a7add64a4eb65149b875caf2cd96
kaskr <kaskr@dtu.dk>
2017-09-22 09:56:04 +0200

    install rgdal

---
+++ install-pkg-rgdal.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+REPOS="https://mirrors.dotsrc.org/cran/"
+
+export LD_LIBRARY_PATH=/appl/proj/4.9.3/lib:$LD_LIBRARY_PATH
+export PKG_CPPFLAGS="-I/appl/proj/4.9.3/include $CPPFLAGS"
+export PKG_LIBS="-Wl,-rpath=/appl/proj/4.9.3/lib -L/appl/proj/4.9.3/lib -lproj $LDFLAGS"
+
+echo "install.packages('rgdal',repos='$REPOS')" | R --vanilla
