0a8e4699e7bffc6521bab2aeb5a51af06a0125a1
kaskr <kaskr@dtu.dk>
2017-09-26 17:13:21 +0200

    patch R wrapper script

--- build_R_MKL.sh
+++ build_R_MKL.sh
@@ -14,6 +14,13 @@ module load intel/2017.4.196.mkl
 wget https://cloud.r-project.org/src/base/R-3/$TARBALL
 tar zxf $TARBALL
 
+## Patch R wrapper script:
+echo "module load intel/2017.4.196.mkl" > tmp_file
+echo "module load gcc/6.3.0" >> tmp_file
+echo >> tmp_file
+cat $RVERSION/src/scripts/R.sh.in >> tmp_file
+mv tmp_file $RVERSION/src/scripts/R.sh.in
+
 MKL="-fopenmp -m64 -I$MKLROOT/include -L$MKLROOT/lib/intel64 -lmkl_gf_lp64 -lmkl_gnu_thread -lmkl_core -lpthread -lm"
 
 mkdir -p build
