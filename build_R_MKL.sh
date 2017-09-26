49157724c51e5c0650afe3b28669bd6998ea21cb
kaskr <kaskr@dtu.dk>
2017-09-26 16:55:05 +0200

    re-structure

--- build_R_MKL.sh
+++ build_R_MKL.sh
@@ -16,7 +16,9 @@ tar zxf $TARBALL
 
 MKL="-fopenmp -m64 -I$MKLROOT/include -L$MKLROOT/lib/intel64 -lmkl_gf_lp64 -lmkl_gnu_thread -lmkl_core -lpthread -lm"
 
-mkdir build
-cd build; ../$RVERSION/configure CPPFLAGS="-I$INCLUDES -I/usr/local/include" LDFLAGS="-L$LIBS -L/usr/local/lib64" --with-blas="$MKL" --with-lapack
+mkdir -p build
 
-make -j 4
+cd build
+../$RVERSION/configure CPPFLAGS="-I$INCLUDES -I/usr/local/include" LDFLAGS="-L$LIBS -L/usr/local/lib64" --with-blas="$MKL" --with-lapack
+
+make
