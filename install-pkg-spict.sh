c003864b216ad94dec8bd8608c0f41dc29f190eb
Tobias Mildenberger <tobi_mildenberger@yahoo.de>
2017-09-29 00:54:19 +0200

    Update install-pkg-spict.sh

--- install-pkg-spict.sh
+++ install-pkg-spict.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-echo "devtools::install_github('mawp/spict/spict')" | R --vanilla
+
+#!/bin/sh
+
+if [ ! -d adcomp ]; then
+    git clone https://github.com/mawp/spict
+fi
+
+cd spict; git pull
+
+make install
+## echo "devtools::install_github('mawp/spict/spict')" | R --vanilla
