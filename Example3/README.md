b1c43e48eab7a154269b97d348bdbdb06c7a538a
kaskr <kaskr@dtu.dk>
2017-09-28 17:07:35 +0200

    stuff

--- Example3/README.md
+++ Example3/README.md
@@ -9,3 +9,4 @@ MC_CORES=2 submit "make -j2 all"
 MAKE="QWAIT=1 submit make" make -j2 all
 ```
 
+MAKE="QWAIT=1 submit make" make -j2 -f Makefile.tmp
--- Example3/modify.sh
+++ Example3/modify.sh
@@ -14,3 +14,9 @@ echo FILE=$FILE > $FILE.tmp
 echo >> $FILE.tmp
 
 sed 's/\t/\n\t$(MAKE) -f $(FILE) /g' tmp3 >> $FILE.tmp
+
+# This can be done using grep:
+#
+# $ cat input.txt | grep --no-group-separator -B 1 '^[^C]'
+#
+# In other words, print any line that doesn't contain C at the start, as well as the previous line [but don't duplicate printed lines]. 
